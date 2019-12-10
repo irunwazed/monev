aaaa0f7405e002e3492d4b73d710c00e9d802e52
irunwazed <irunwazed@gmail.com>
2019-12-10 22:45:21 +0800

    aka 1.8

--- plaining/application/views/monev/include/head.php
+++ plaining/application/views/monev/include/head.php
@@ -9,7 +9,7 @@ if(!@$_SESSION['id']){
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>E-Monev | Morowali</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
